3b5b035dde17cce0db93a3479d3ae0a55eee0693
Rodrigo Spinelli <rodrigo.spinelli9@gmail.com>
2026-09-20 01:56:56 +0100

    feat: implement Zod validation in create account controller

---
+++ src/pipes/zod-validation-pipe.ts
@@ -0,0 +1,26 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+import { ZodError, ZodObject } from 'zod';
+import { fromZodError } from 'zod-validation-error';
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodObject<any>) {}
+
+  transform(value: unknown, metadata: ArgumentMetadata) {
+    try {
+      this.schema.parse(value);
+    } catch (error) {
+      if (error instanceof ZodError)
+        throw new BadRequestException({
+          message: 'Validation failed',
+          statusCode: 400,
+          errors: fromZodError(error),
+        });
+      throw new BadRequestException('Method not implemented.');
+    }
+    return value;
+  }
+}
