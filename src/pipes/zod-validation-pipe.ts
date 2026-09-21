9f70109cb967f2b65bc8fd7578f46a4856291cd5
Rodrigo Spinelli <rodrigo.spinelli9@gmail.com>
2026-09-21 09:01:00 +0100

    refactor: update Zod validation pipe to use ZodSchema for improved type safety

--- src/pipes/zod-validation-pipe.ts
+++ src/pipes/zod-validation-pipe.ts
@@ -3,15 +3,15 @@ import {
   BadRequestException,
   PipeTransform,
 } from '@nestjs/common';
-import { ZodError, ZodObject } from 'zod';
+import { ZodError, ZodSchema } from 'zod';
 import { fromZodError } from 'zod-validation-error';
 
 export class ZodValidationPipe implements PipeTransform {
-  constructor(private schema: ZodObject<any>) {}
+  constructor(private schema: ZodSchema) {}
 
   transform(value: unknown, metadata: ArgumentMetadata) {
     try {
-      this.schema.parse(value);
+      return this.schema.parse(value);
     } catch (error) {
       if (error instanceof ZodError)
         throw new BadRequestException({
